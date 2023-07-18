1acf872977c368ddd45bd1bbb61df12b5bcc359f
Imotytskiy <Imotytskiy@gmail.com>
2023-07-18 11:59:40 +0200

    add alpha 0.9

--- my-app/src/components/Nav/Navleft.jsx
+++ my-app/src/components/Nav/Navleft.jsx
@@ -3,6 +3,8 @@ import Nav from "react-bootstrap/Nav";
 import "./Navleft.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const cities = ["KYIV", "BERLIN", "PARIS", "MADRID", "ROME", "WARSAWA"];
+
 function Navleft({ onEventKeyChange }) {
   const [activeKey, setActiveKey] = useState("/home");
 
@@ -13,25 +15,15 @@ function Navleft({ onEventKeyChange }) {
 
   return (
     <Nav variant="pills" defaultActiveKey={activeKey} className="flex-column">
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="0" onClick={() => handleNavClick("0")}>
-        KYIV
-      </Nav.Link>
-      <Nav.Link eventKey="1" onClick={() => handleNavClick("1")}>
-        BERLIN
-      </Nav.Link>
-      <Nav.Link eventKey="2" onClick={() => handleNavClick("2")}>
-        PARIS
-      </Nav.Link>
-      <Nav.Link eventKey="3" onClick={() => handleNavClick("3")}>
-        MADRID
-      </Nav.Link>
-      <Nav.Link eventKey="4" onClick={() => handleNavClick("4")}>
-        ROME
-      </Nav.Link>
-      <Nav.Link eventKey="5" onClick={() => handleNavClick("5")}>
-        WARSAWA
-      </Nav.Link>
+      {cities.map((city, index) => (
+        <Nav.Link
+          key={index}
+          eventKey={index.toString()}
+          onClick={() => handleNavClick(index.toString())}
+        >
+          {city}
+        </Nav.Link>
+      ))}
     </Nav>
   );
 }
